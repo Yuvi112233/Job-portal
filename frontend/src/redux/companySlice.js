f2017aa611ab378550b96e177d8e735df9f158c3
Yuvraj Kaushik <yuvrajkaushik57@gmail.com>
2025-04-20 12:39:17 +0530

    fixes

--- frontend/src/redux/companySlice.js
+++ frontend/src/redux/companySlice.js
@@ -1,24 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const companySlice = createSlice({
-    name:"company",
-    initialState:{
-        singleCompany:null,
-        companies:[],
-        searchCompanyByText:"",
+    name: "company",
+    initialState: {
+        singleCompany: null,
+        companies: [],
+        searchCompanyByText: "",
     },
-    reducers:{
-        // actions
-        setSingleCompany:(state,action) => {
+    reducers: {
+        // Actions
+        setSingleCompany: (state, action) => {
             state.singleCompany = action.payload;
         },
-        setCompanies:(state,action) => {
+        setCompanies: (state, action) => {
             state.companies = action.payload;
         },
-        setSearchCompanyByText:(state,action) => {
+        setSearchCompanyByText: (state, action) => {
             state.searchCompanyByText = action.payload;
-        }
-    }
+        },
+        // Add setCompanyDetails action here
+        setCompanyDetails: (state, action) => {
+            state.singleCompany = action.payload;
+        },
+    },
 });
-export const {setSingleCompany, setCompanies,setSearchCompanyByText} = companySlice.actions;
+
+export const { setSingleCompany, setCompanies, setSearchCompanyByText, setCompanyDetails } = companySlice.actions;
 export default companySlice.reducer;
